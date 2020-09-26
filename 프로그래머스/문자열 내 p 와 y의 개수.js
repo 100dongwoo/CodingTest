f3915c6c77b5647add480f0a0f4d7ef8cc2c25db
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-26 14:04:18 +0900

    문자열 내 마음대로 정렬하기 level 1

--- 프로그래머스/문자열 내 p 와 y의 개수.js
+++ 프로그래머스/문자열 내 p 와 y의 개수.js
@@ -2,7 +2,6 @@ function solution(s) {
     var answer = true;
     let countP = 0;
     let countY = 0;
-
     for (let i = 0; i < s.length; i++) {
         if (s[i] === 'P' || s[i] === 'p')
             countP++;
---
+++ 프로그래머스/문자열 내 마음대로 정렬하기 level1.js
@@ -0,0 +1,39 @@
+function solution(strings, n) {
+    var answer = [];
+    for(let i = 0; i<strings.length; i++){
+        for(let j = i+1; j<strings.length; j++){
+            if(strings[i][n] > strings[j][n]){
+                [strings[i], strings[j]] = [strings[j], strings[i]];
+            }
+            if(strings[i][n] === strings[j][n]){
+                if(strings[i] > strings[j]){
+                    [strings[i], strings[j]] = [strings[j], strings[i]];
+                }
+            }
+        }
+    }
+    answer = strings;
+    return answer;
+}
+
+//best 답안
+// 1
+// function solution(strings, n) {
+//     // strings 배열
+//     // n 번째 문자열 비교
+//     return strings.sort((s1, s2) => s1[n] === s2[n] ? s1.localeCompare(s2) : s1[n].localeCompare(s2[n]));
+// }
+
+//2
+// function solution(strings, n) {
+//     return strings.sort((a, b) => {
+//         const chr1 = a.charAt(n);
+//         const chr2 = b.charAt(n);
+//
+//         if (chr1 == chr2) {
+//             return (a > b) - (a < b);
+//         } else {
+//             return (chr1 > chr2) - (chr1 < chr2);
+//         }
+//     })
+// }
