edff8c352de95c7491c5cc7521ec1813021a1ade
BAEK DONGWOO <qorehddn123@naver.com>
2020-09-27 13:10:51 +0900

    문자열 다루기 기본

---
+++ 프로그래머스/문자열 다루기 기본 level1.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = true;
+    if(s.length!==4 && s.length!==6)
+    {
+        return false
+    }
+    for(let i=0;i<s.length;i++)
+    {
+        if(s[i]<'0' || s[i]>'9')
+            return false
+    }
+    return answer;
+
+}
