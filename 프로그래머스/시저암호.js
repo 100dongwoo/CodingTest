afdcbdb0c4548df0a3dfda46d307511aab759c22
BAEK DONGWOO <qorehddn123@naver.com>
2020-10-16 00:41:13 +0900

    시저암호

---
+++ 프로그래머스/시저암호.js
@@ -0,0 +1,28 @@
+function solution(s, n) {
+    var answer = '';
+    // console.log('a'.charCodeAt(0))   97
+    // console.log('z'.charCodeAt(0))   122
+    //   console.log('Z'.charCodeAt(0)) 90
+    //   console.log('A'.charCodeAt(0))    65
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] === " ") {
+            answer += " "
+        } else {
+
+            if (s.charCodeAt(i) + n > 122 && s.charCodeAt(i) > 96) //z 이상이면
+            {
+
+                answer += String.fromCharCode((s.charCodeAt(i) + n) % 122 + 96)
+
+            } else if (s.charCodeAt(i) + n > 90 && s.charCodeAt(i) < 97) {
+
+                answer += String.fromCharCode(((s.charCodeAt(i) + n) % 90) + 64)
+            } else {
+
+                answer += String.fromCharCode(s.charCodeAt(i) + n)
+            }
+        }
+    }
+    return answer;
+}
