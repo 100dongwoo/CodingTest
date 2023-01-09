1d8b5d66eb4498bfd2affc055c0e950d2bd09c17
100dongwoo <qorehddn123@naver.com>
2023-01-09 17:58:26 +0900

    신고 결과 받기

--- 프로그래머스/신고 결과 받기.js
+++ 프로그래머스/신고 결과 받기.js
@@ -1,12 +1,14 @@
 function solution(id_list, report, k) {
   var answer = new Array(id_list.length).fill(0);
-  const countObj = {};
   const set = new Set(report);
   const uniqueArr = [...set];
   const arr = uniqueArr.map((data) => data.split(" "));
+
+  const countObj = {};
   for (const v of arr) {
-    countObj[v[1]] = countObj[v[1]] ? countObj[v[1]] + 1 : 1;
+    countObj[v[1]] = countObj[v[1]] + 1 || 1;
   }
+
   id_list.map((data, index) => {
     arr.map((report) => {
       if (data === report[0] && countObj[report[1]] >= k) {
