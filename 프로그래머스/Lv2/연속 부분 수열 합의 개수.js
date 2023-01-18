f56a121d5cb33250a618d7b369ea7424b63e7e96
100dongwoo <qorehddn123@naver.com>
2023-01-18 18:24:41 +0900

    연속 부분 수열 합의 개수

--- 프로그래머스/Lv2/연속 부분 수열 합의 개수.js
+++ 프로그래머스/Lv2/연속 부분 수열 합의 개수.js
@@ -6,13 +6,13 @@ function solution(elements) {
       0
     );
   };
+  let elementsCopy = elements.concat(elements);
+
   for (let i = 1; i <= elements.length; i++) {
-    const els = elements.concat(elements.slice(0, i));
     for (let j = 0; j < elements.length; j++) {
-      result.push(sumWith(els.slice(j, j + i)));
+      result.push(sumWith(elementsCopy.slice(i - 1, i + j)));
     }
   }
   const set = new Set(result);
-
   return [...set].length;
 }
