32e11dfad3669b9ad05abbfc366c650d1eacca23
100dongwoo <qorehddn123@naver.com>
2023-02-01 12:18:43 +0900

    이진 변환 반복하기

--- 프로그래머스/Lv2/이진 변환 반복하기.js
+++ 프로그래머스/Lv2/이진 변환 반복하기.js
@@ -1,32 +1,10 @@
-function solution(info, query) {
-  var answer = [];
-
-  info = info.map((data) => data.split(" "));
-  query = query.map((data) => data.split(" and "));
-
-  query.map((queryData) => {
-    let count = 0;
-    for (let i = 0; i < info.length; i++) {
-      if (info[i][0] === queryData[0] || queryData[0] === "-") {
-        if (info[i][1] === queryData[1] || queryData[1] === "-") {
-          if (info[i][2] === queryData[2] || queryData[2] === "-") {
-            let value = queryData[3].split(" ");
-            if (info[i][3] === value[0] || value[0] === "-") {
-              if (info[i][4] >= parseInt(value[1]) || value[1] === "-") {
-                count++;
-              }
-              break;
-            }
-            break;
-          }
-          break;
-        }
-        break;
-      }
-      break;
-    }
-
-    answer.push(count);
-  });
+function solution(s) {
+  let answer = [0, 0];
+  while (s !== "1") {
+    let value = s.split("0").length - 1;
+    s = (s.length - value).toString(2);
+    answer[1] += value;
+    answer[0] += 1;
+  }
   return answer;
 }
