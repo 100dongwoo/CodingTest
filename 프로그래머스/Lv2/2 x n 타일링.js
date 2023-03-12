bc0a09734c87109a8b4035f9a945350fd77aa9b0
100dongwoo <qorehddn123@naver.com>
2023-03-12 21:03:51 +0900

    2 x n 타일링

---
+++ 프로그래머스/Lv2/2 x n 타일링.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  let list = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    if (!list[i]) {
+      list.push((list[i - 2] + list[i - 1]) % 1000000007);
+    }
+  }
+  return list[n - 1];
+}
