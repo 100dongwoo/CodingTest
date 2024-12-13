57fab569e6be4e96f1c8ac31fcc2f473796b4938
100dongwoo <qorehddn123@naver.com>
2024-12-13 19:51:24 +0900

    feat : 주식가격LV2(수정결과 추가)

--- 프로그래머스/Lv2/주식가격.js
+++ 프로그래머스/Lv2/주식가격.js
@@ -16,3 +16,20 @@ function solution(prices) {
   }
   return answer;
 }
+
+// ** 수정적용 (한개의 변수로 사용하여 가독성 )
+function solution(prices) {
+  var answer = [];
+  const totalLength = prices.length;
+  for (let i = 0; i < totalLength; i++) {
+    let times = 0;
+    for (let j = i + 1; j < totalLength; j++) {
+      times++;
+      if (prices[i] > prices[j]) {
+        break;
+      }
+    }
+    answer.push(times);
+  }
+  return answer;
+}
